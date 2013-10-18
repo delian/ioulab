e30ab02c8725fe767ae6847ccac14756d40feb32
Delian Delchev <delian.delchev@gmail.com>
2013-10-18 23:34:21 +0000

    Network connectivity update
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- app.js
+++ app.js
@@ -65,5 +65,6 @@ mongoClient.connect(config.mongoUrl,function(err,mongoDb) {
 		sIo = io.listen(server, { log: false });
 		require('./lib/labSocketBus.js')(sIo,db,e);
 		require('./lib/termSocketBus.js')(sIo,db,e);
+		require('./lib/vncSocketBus.js')(sIo,db,e);
 	});
 });
